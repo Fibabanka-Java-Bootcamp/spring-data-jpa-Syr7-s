f1649396e01034f600d6b5f3ed22deb0caa99dea
Syr7-s <isasayar7@github.com>
2021-01-18 15:51:41 +0300

    Pom.xml was edited. Some necessary fields were added to the Book Class.

--- src/main/java/org/kodluyoruz/entities/Book.java
+++ src/main/java/org/kodluyoruz/entities/Book.java
@@ -1,6 +1,7 @@
 package org.kodluyoruz.entities;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 public class Book {
@@ -9,6 +10,20 @@ public class Book {
     private String isbn;
 
     private String name;
+
+    private String description;
+
+    private LocalDate publishedDate;
+
+    private LocalDate addedDate;
+
+    private double price;
+
+    private String currency;
+
+    private String imageUrl;
+
+
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "category_id", referencedColumnName = "id")
@@ -38,11 +53,74 @@ public class Book {
         this.category = category;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public LocalDate getAddedDate() {
+        return addedDate;
+    }
+
+    public void setAddedDate(LocalDate addedDate) {
+        this.addedDate = addedDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+ /*   @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }*/
+
     @Override
     public String toString() {
         return "Book{" +
                 "isbn='" + isbn + '\'' +
                 ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", publishedDate=" + publishedDate +
+                ", addedDate=" + addedDate +
+                ", price=" + price +
+                ", currency='" + currency + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                //", category=" + category +
                 '}';
     }
 }
