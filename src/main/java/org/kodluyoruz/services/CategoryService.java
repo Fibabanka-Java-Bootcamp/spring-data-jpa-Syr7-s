bf5c853dc5f7b87c8e6b82b66891f62537bf15f6
Syr7-s <isasayar7@github.com>
2021-01-18 15:59:50 +0300

    Book fields was filled.

--- src/main/java/org/kodluyoruz/services/CategoryService.java
+++ src/main/java/org/kodluyoruz/services/CategoryService.java
@@ -5,6 +5,7 @@ import org.kodluyoruz.entities.Category;
 import org.kodluyoruz.repositories.CategoryRepo;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.UUID;
 
@@ -22,6 +23,12 @@ public class CategoryService {
         Book b1 = new Book();
         b1.setIsbn(UUID.randomUUID().toString());
         b1.setName("Spring in Action");
+        b1.setDescription("This book about Spring");
+        b1.setPublishedDate(LocalDate.of(1998,8,10));
+        b1.setAddedDate(LocalDate.of(2002,10,15));
+        b1.setPrice(150.45);
+        b1.setCurrency("₺");
+        b1.setImageUrl("image1.jpg");
 
         Category c1 = new Category();
         c1.setName("Computer Science");
