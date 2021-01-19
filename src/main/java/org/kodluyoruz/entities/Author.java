9625a61e59af6bb04456f68284c69ba692da56e5
Syr7-s <isasayar7@github.com>
2021-01-19 14:42:15 +0300

    Lombok annotations (Getter-Setter) were added to the Author Class

--- src/main/java/org/kodluyoruz/entities/Author.java
+++ src/main/java/org/kodluyoruz/entities/Author.java
@@ -1,16 +1,22 @@
 package org.kodluyoruz.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Author {
     @Id
     @GeneratedValue
     private int id;
 
     private String name;
+/*
 
     public int getId() {
         return id;
@@ -27,7 +33,7 @@ public class Author {
     public void setName(String name) {
         this.name = name;
     }
-
+*/
     @Override
     public String toString() {
         return "Author{" +
