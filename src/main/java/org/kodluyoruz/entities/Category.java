0ec22402f6f58769a816d336f00a32c34de9378e
Syr7-s <isasayar7@github.com>
2021-01-19 14:44:13 +0300

    Category class was edited.

--- src/main/java/org/kodluyoruz/entities/Category.java
+++ src/main/java/org/kodluyoruz/entities/Category.java
@@ -19,31 +19,7 @@ public class Category {
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
     private List<Book> books;
-/*
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Book> getBooks() {
-        return books;
-    }
-
-    public void setBooks(List<Book> books) {
-        this.books = books;
-    }
-*/
     @Override
     public String toString() {
         return "Category{" +
