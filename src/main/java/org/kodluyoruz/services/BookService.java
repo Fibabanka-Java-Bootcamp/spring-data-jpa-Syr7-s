6e77d5e509c9a52169249e5d2be98143fadbf84e
Syr7-s <isasayar7@github.com>
2021-01-19 19:18:42 +0300

    Book Service was edited.

--- src/main/java/org/kodluyoruz/services/BookService.java
+++ src/main/java/org/kodluyoruz/services/BookService.java
@@ -23,40 +23,48 @@ public class BookService {
 
     public void bookOperation() {
         Author author = new Author();
-        author.setName("Laurentiu Spilca");
+        author.setName("Tolstoy");
 
         Book book = new Book();
         book.setIsbn(UUID.randomUUID().toString());
-        book.setName("Spring Security");
-        book.setDescription("This book about Spring");
-        book.setPublishedDate(LocalDate.of(2019, 7, 11));
-        book.setAddedDate(LocalDate.of(2021, 1, 1));
+        book.setName("Savas Baris");
+        book.setDescription("The Roman");
+        book.setPublishedDate(LocalDate.of(1867, 7, 11));
+        book.setAddedDate(LocalDate.of(2002, 1, 1));
         book.setPrice(180.78);
         book.setCurrency("₺");
         book.setImageUrl("image4.jpg");
+
+        Book book1 = new Book();
+        book1.setIsbn(UUID.randomUUID().toString());
+        book1.setName("Anna Karina");
+        book1.setDescription("The Roman");
+        book1.setPublishedDate(LocalDate.of(1877, 8, 23));
+        book1.setAddedDate(LocalDate.of(2004, 1, 1));
+        book1.setPrice(190.45);
+        book1.setCurrency("₺");
+        book1.setImageUrl("image5.jpg");
         List<Author> authorList = new ArrayList<>();
         authorList.add(author);
         book.setAuthors(authorList);
 
         List<Book> books = new ArrayList<>();
         books.add(book);
+        books.add(book1);
         author.setRegisteredAuthorBook(books);
         System.out.println("Authors from Book Service");
         authorRepo.save(author);
         bookRepo.save(book);
+        bookRepo.save(book1);
         System.out.println(author);
 
        /* System.out.println("Book from Book Service");
         System.out.println(book);*/
-        Book book1 = bookRepo.findBookByName("Spring Security");
-        System.out.println(book1);
+        Book book2 = bookRepo.findBookByName("Savas Baris");
+        System.out.println(book2);
         System.out.println("**********************************");
-        Author book1Author= authorRepo.findAuthorByName("Laurentiu Spilca");
+        Author book1Author= authorRepo.findAuthorByName("Tolstoy");
         System.out.println(book1Author.getRegisteredAuthorBook());
-        /*
-        System.out.println("Find Book By Author Name");
 
-        Author bookAuthor = bookRepo.findBookByAuthors(authorList);
-        System.out.println(bookAuthor.getName());*/
     }
 }
