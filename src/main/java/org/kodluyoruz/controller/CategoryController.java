f736e533884424b0a229b7d4d639ced558277939
Syr7-s <isasayar7@github.com>
2021-01-22 20:15:20 +0300

    CategoryController was edited.

--- src/main/java/org/kodluyoruz/controller/CategoryController.java
+++ src/main/java/org/kodluyoruz/controller/CategoryController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 import java.util.List;
 @RestController
 @RequestMapping("/api/category")
@@ -23,20 +25,21 @@ public class CategoryController {
     @RequestMapping(value = "/addCategory",method = RequestMethod.GET)
     public String addCategory(){
         categoryService.categoryAddToTheDatabase();
-        return "Yeni kategori eklendi.";
+        return "New category was added";
     }
 
     @RequestMapping(value = "/{name}",method = RequestMethod.GET)
     public String getCategory(@PathVariable("name") String name){
         Category category = categoryService.getCategory(name);
         System.out.println(category);
-        return category.getName()+" adli kategori getirildi.";
+        return category.getName()+" named category was received";
     }
 
     @RequestMapping(value = "/allCategory",method = RequestMethod.GET)
     public String getAllCategory(){
         List<Category> categoryList = categoryService.getAllCategory();
-        System.out.println(categoryList);
-        return "Tum kategoriler getirildi.";
+        System.out.println("All categeries are getting.");
+        Arrays.stream(categoryList.toArray()).forEach(System.out::println);
+        return "All categories have been brought.";
     }
 }
