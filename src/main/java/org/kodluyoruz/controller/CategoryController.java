a72d482fe69b46ceb8d75bfc177675087a6f9e09
Syr7-s <isasayar7@github.com>
2021-01-23 14:12:31 +0300

    CategoryController was edited.

--- src/main/java/org/kodluyoruz/controller/CategoryController.java
+++ src/main/java/org/kodluyoruz/controller/CategoryController.java
@@ -3,10 +3,12 @@ package org.kodluyoruz.controller;
 import org.kodluyoruz.entities.Category;
 import org.kodluyoruz.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Arrays;
 import java.util.List;
@@ -35,18 +37,18 @@ public class CategoryController {
         if (category != null) {
             System.out.println(category);
             return category.getName() + " named category was received";
-        }
-        return name+" named category is not found";
+        }throw new ResponseStatusException(HttpStatus.NOT_FOUND, name+" named category is not found");
+       // return name+" named category is not found";
 
     }
 
-    @RequestMapping(value = "/allCategory", method = RequestMethod.GET)
+    @RequestMapping(value = "/categories", method = RequestMethod.GET)
     public String getAllCategory() {
         List<Category> categoryList = categoryService.getAllCategory();
         if (!categoryList.isEmpty()) {
             System.out.println("All categeries are getting.");
             Arrays.stream(categoryList.toArray()).forEach(System.out::println);
             return "All categories have been brought.";
-        }return "CategoryList is empty";
+        }throw new ResponseStatusException(HttpStatus.NOT_FOUND,"CategoryList is empty");//return "CategoryList is empty";
     }
 }
