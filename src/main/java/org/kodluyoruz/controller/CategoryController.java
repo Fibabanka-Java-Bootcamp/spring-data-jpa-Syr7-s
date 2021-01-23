4a58e7cca4bbac04671558d55d3c20f3773c290d
Syr7-s <isasayar7@github.com>
2021-01-23 16:35:24 +0300

    CategoryController was edited.

--- src/main/java/org/kodluyoruz/controller/CategoryController.java
+++ src/main/java/org/kodluyoruz/controller/CategoryController.java
@@ -4,10 +4,7 @@ import org.kodluyoruz.entities.Category;
 import org.kodluyoruz.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Arrays;
@@ -16,37 +13,38 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/category")
 public class CategoryController {
-    @Autowired
-    private CategoryService categoryService;
+   // @Autowired
+    private final CategoryService categoryService;
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
 
-    @RequestMapping(value = "/categoryHome", method = RequestMethod.GET)
+    @GetMapping("/home")
     public String getCategoryHome() {
         categoryService.categoryOperations();
         return "Category Operation is running";
     }
 
-    @RequestMapping(value = "/addCategory", method = RequestMethod.GET)
+    @GetMapping("/newCategory")
     public String addCategory() {
         categoryService.categoryAddToTheDatabase();
         return "New category was added";
     }
 
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    public String getCategory(@PathVariable("name") String name) {
+    @GetMapping
+    public String getCategory(@RequestParam("name") String name) {
         Category category = categoryService.getCategory(name);
         if (category != null) {
             System.out.println(category);
             return category.getName() + " named category was received";
         }throw new ResponseStatusException(HttpStatus.NOT_FOUND, name+" named category is not found");
-       // return name+" named category is not found";
-
     }
 
-    @RequestMapping(value = "/categories", method = RequestMethod.GET)
+    @GetMapping(value = "/categories")
     public String getAllCategory() {
         List<Category> categoryList = categoryService.getAllCategory();
         if (!categoryList.isEmpty()) {
-            System.out.println("All categeries are getting.");
+            System.out.println("All categories are getting.");
             Arrays.stream(categoryList.toArray()).forEach(System.out::println);
             return "All categories have been brought.";
         }throw new ResponseStatusException(HttpStatus.NOT_FOUND,"CategoryList is empty");//return "CategoryList is empty";
